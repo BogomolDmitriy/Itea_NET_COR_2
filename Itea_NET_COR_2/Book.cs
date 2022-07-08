652bc8542d4249b716b5488ced1b723f53ebb804
BogomolDmitriy <bogomoldima1992@gmail.com>
2022-07-08 14:13:56 +0300

    Implemented Boos Controller methods

---
+++ Itea_NET_COR_2/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Itea_NET_COR_2
+{
+    public class Book
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Autor { get; set; }
+        public int PegesCount { get; set; }
+    }
+}
